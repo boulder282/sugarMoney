9902ed2e0911ad47bfcfe2cadaf9d29eb358c228
Danila <tvorvor2013@gmail.com>
2026-06-29 23:11:17 +0400

    [Sugar_Money]02 fix: GH base URL fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./app/App"
 import { ThemeProvider } from "./app/providers/ThemeProvider"
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/sugarMoney/">
     <ThemeProvider>
       <App />
     </ThemeProvider>
